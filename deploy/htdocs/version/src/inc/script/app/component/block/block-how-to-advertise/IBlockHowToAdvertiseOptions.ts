26ff3cb089428bb72175d3bfb6a64c9b113aec62
Lars van Braam <lars@mediamonks.com>
2017-01-06 14:25:59 +0100

    Add icons

--- deploy/htdocs/version/src/inc/script/app/component/block/block-how-to-advertise/IBlockHowToAdvertiseOptions.ts
+++ deploy/htdocs/version/src/inc/script/app/component/block/block-how-to-advertise/IBlockHowToAdvertiseOptions.ts
@@ -1,7 +1,15 @@
 import IDefaultComponentOptions from "../IDefaultComponentOptions";
+import ILink from "../../../data/interface/action/ILink";
 
-export interface IBlockHowToAdvertiseOptions extends IDefaultComponentOptions {
-
+export interface IBlockHowToAdvertiseOptions extends IDefaultComponentOptions
+{
+	heading: string;
+	link: ILink;
+	steps: Array<{
+		heading: string;
+		paragraph: string;
+		icon: string;
+	}>
 }
 
 export default IBlockHowToAdvertiseOptions;
