31d162163ee491801e6872502b016f21ae8a3ee9
Jose Sanchez <jose.sanchez@mediamonks.com>
2017-01-09 13:42:52 -0300

    Add block Latest News.

---
+++ deploy/htdocs/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/NodeArticleTeaserV1.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\spectrum_rest\Plugin\RestEntityProcessor;
+
+use Drupal\mm_rest\Plugin\RestEntityProcessorBase;
+
+/**
+ * Returns the structured data of an entity.
+ *
+ * @RestEntityProcessor(
+ *   id = "spectrum_rest_node_article_teaser_v1",
+ *   label = @Translation("Node: Article"),
+ *   version = "v1",
+ *   entity_type = "node",
+ *   bundle = "article",
+ *   view_mode = "teaser"
+ * )
+ */
+class NodeArticleTeaserV1 extends RestEntityProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getItemData($entity) {
+
+    $data = [
+      "title" => $entity->label(),
+      "excerpt" => $this->fieldProcessor->getFieldData($entity->field_excerpt),
+      "image" => $this->fieldProcessor->getFieldData($entity->field_image),
+      "tags" => $this->fieldProcessor->getFieldData($entity->field_tags),
+    ];
+
+    return $data;
+  }
+
+}
---
+++ deploy/htdocs/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/TaxonomyTermTagsV1.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\spectrum_rest\Plugin\RestEntityProcessor;
+
+use Drupal\mm_rest\Plugin\RestEntityProcessorBase;
+
+/**
+ * Returns the structured data of an entity.
+ *
+ * @RestEntityProcessor(
+ *   id = "spectrum_rest_taxonomy_term_tags_v1",
+ *   label = @Translation("Taxonomy term: Tags"),
+ *   version = "v1",
+ *   entity_type = "taxonomy_term",
+ *   bundle = "tags",
+ *   view_mode = "default"
+ * )
+ */
+class TaxonomyTermTagsV1 extends RestEntityProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getItemData($entity) {
+
+    $data = [
+      "id" => $entity->id(),
+      "name" => $entity->label(),
+    ];
+
+    return $data;
+  }
+
+}
