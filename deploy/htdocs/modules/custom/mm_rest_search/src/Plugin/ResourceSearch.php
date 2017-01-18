c0582ce7cd12b786d61d4860be279034f60f9ca8
Jose Sanchez <jose.sanchez@mediamonks.com>
2017-01-18 17:11:38 -0300

    Add the mm_rest_search module.

---
+++ deploy/htdocs/modules/custom/mm_rest_search/src/Plugin/ResourceSearch.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace Drupal\mm_rest_search\Plugin;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\facets\Exception\Exception;
+use Drupal\mm_rest\Plugin\ResourceBase as MMResourceBase;
+use Drupal\search_api\Entity\Index;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\mm_rest\CacheableMetaDataCollectorInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\mm_rest\Plugin\RestEntityProcessorManager;
+
+use Drupal\facets\FacetManager\DefaultFacetManager;
+
+abstract class ResourceSearch extends MMResourceBase {
+
+  /**
+   * The default facet manager.
+   *
+   * @var \Drupal\facets\FacetManager\DefaultFacetManager
+   */
+  protected $facetsManager;
+
+  /**
+   * Returns the search results.
+   *
+   * @return array
+   * @throws \Drupal\facets\Exception\Exception
+   *
+   * @TODO: refactor, separate into different protected functions.
+   */
+  public function get() {
+    $results = [];
+    $request = \Drupal::request();
+    $index = $this->getIndex();
+
+    if (empty($index)) {
+      throw new Exception($this->t('There is no index defined'));
+    }
+
+    /* @var $search_api_index \Drupal\search_api\IndexInterface */
+    $search_api_index = Index::load($index);
+
+    if (empty($search_api_index)) {
+      throw new Exception($this->t("Index '@index' does not exist.", ['@index' => $index]));
+    }
+
+    // Create the query.
+    /** @var \Drupal\search_api\Query\QueryInterface $query */
+    $query = $search_api_index->query([
+      'limit' => $this->getLimit(),
+      'offset' => !is_null($request->get('page')) ? $request->get('page') * $this->getLimit() : 0,
+    ]);
+
+    $search_id = "mm_rest:" . $this->getPluginId();
+
+    $query->setSearchId($search_id);
+
+    // Search for keys.
+    $keys = $request->get('query');
+    if (!empty($keys)) {
+      $query->keys($keys);
+    }
+
+    $result = $query->execute();
+    $items = $result->getResultItems();
+
+    foreach ($items as $item) {
+      $entity = $item->getOriginalObject()->getValue();
+      $results[] = $this->entityProcessor->getEntityData($entity, $this->getVersion(), ['view_mode' => $this->getViewMode()]);
+    }
+
+    // Processing facets.
+    $facets = $this->facetsManager->getFacetsByFacetSourceId($search_id);
+    $this->facetsManager->updateResults($search_id);
+
+    $filters = [];
+    foreach ($facets as $facet) {
+      $filter = $this->facetsManager->build($facet);
+      $filter = is_array($filter) ? reset($filter) : $filter;
+      $filters = array_merge($filters, $filter);
+    }
+
+    $data = [
+      'results' => $results,
+      'filters' => $filters,
+    ];
+
+    $this->addCacheableDependency();
+
+    return $data;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getViewMode() {
+    $definition = $this->getPluginDefinition();
+    return isset($definition['view_mode']) ? $definition['view_mode'] : 'default';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIndex() {
+    $definition = $this->getPluginDefinition();
+    return isset($definition['index_name']) ? $definition['index_name'] : NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLimit() {
+    $definition = $this->getPluginDefinition();
+    return isset($definition['search_limit']) ? $definition['search_limit'] : 10;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Disable the cache.
+   */
+  protected function addCacheableDependency() {
+
+    // If $this::requestData contains a content entity, the is no need to
+    // override this method. The default will do.
+    parent::addCacheableDependency();
+
+    // If more cacheability meta data must be collected, use for example:
+    // $this->cacheabilityCollector->addCacheableDependency($entity);
+
+    // To disable caching, set the max age to 0 seconds.
+    $meta_data = new CacheableMetadata();
+    $meta_data->setCacheMaxAge(0);
+    $this->cacheabilityCollector->addCacheableDependency($meta_data);
+  }
+
+  /**
+   * ResourceSearch constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request.
+   * @param \Drupal\mm_rest\Plugin\RestEntityProcessorManager $entity_processor
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   * @param \Drupal\mm_rest\CacheableMetaDataCollectorInterface $cacheable_metadata_collector
+   * @param \Drupal\facets\FacetManager\DefaultFacetManager $facet_manager
+  */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger, Request $request, RestEntityProcessorManager $entity_processor, ConfigFactoryInterface $configFactory, CacheableMetaDataCollectorInterface $cacheable_metadata_collector, DefaultFacetManager $facet_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger, $request, $entity_processor, $configFactory, $cacheable_metadata_collector);
+    $this->facetsManager= $facet_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('mm_rest'),
+      $container->get('request_stack')->getCurrentRequest(),
+      $container->get('plugin.manager.mm_rest_entity_processor'),
+      $container->get('config.factory'),
+      $container->get('mm_rest.cacheable_metadata_collector'),
+      $container->get('facets.manager')
+    );
+  }
+
+}
---
+++ deploy/htdocs/modules/custom/mm_rest_search/src/Plugin/facets/facet_source/SearchApiRestDeriver.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\mm_rest_search\Plugin\facets\facet_source;
+
+use Drupal\rest\Plugin\Type\ResourcePluginManager;
+use Drupal\facets\FacetSource\FacetSourceDeriverBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Derives a facet source plugin definition for every Search API display plugin.
+ *
+ * This facet source supports all search api display sources.
+ *
+ * @see \Drupal\facets\Plugin\facets\facet_source\SearchApi
+ */
+class SearchApiRestDeriver extends FacetSourceDeriverBase {
+
+  /**
+   * @var \Drupal\rest\Plugin\Type\ResourcePluginManager
+   */
+  protected $pluginManagerRest;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    /** @var static $deriver */
+    $deriver = parent::create($container, $base_plugin_id);
+
+    /** @var \Drupal\rest\Plugin\Type\ResourcePluginManager $plugin_manager_rest */
+    $plugin_manager_rest = $container->get('plugin.manager.rest');
+    $deriver->setPluginManagerRest($plugin_manager_rest);
+
+    return $deriver;
+  }
+
+  /**
+   * Sets the plugin manager rest.
+   *
+   * @param \Drupal\rest\Plugin\Type\ResourcePluginManager $plugin_manager_rest
+   *   The plugin manager rest.
+   *
+   * @return $this
+   */
+  public function setPluginManagerRest(ResourcePluginManager $plugin_manager_rest) {
+    $this->pluginManagerRest = $plugin_manager_rest;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $base_plugin_id = $base_plugin_definition['id'];
+
+    $plugins = $this->pluginManagerRest->getDefinitions();
+
+    foreach ($plugins as $plugin) {
+      if (isset($plugin['index_name']) && isset($plugin['uri_paths']['canonical'])) {
+        $plugin_derivatives = array();
+        $machine_name = $plugin['id'];
+        $plugin_derivatives[$machine_name] = [
+            'id' => $base_plugin_id . ':' . $machine_name,
+            'label' => $plugin['label'],
+            'description' => $this->t('Provides a facet source.'),
+            'index_name' => $plugin['index_name'],
+            'resource_id' => $machine_name,
+            'path' => $plugin['uri_paths']['canonical'],
+          ] + $base_plugin_definition;
+
+        $this->derivatives[$base_plugin_id] = $plugin_derivatives;
+      }
+    }
+
+    return $this->derivatives[$base_plugin_id];
+  }
+
+}
