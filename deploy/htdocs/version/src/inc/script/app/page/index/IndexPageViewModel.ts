a2cefd26cf3b33d76a9d5d8ac2a8755d29e9dde2
Lars van Braam <lars@mediamonks.com>
2017-01-18 10:36:59 +0100

    Added localized buttons

--- deploy/htdocs/version/src/inc/script/app/page/index/IndexPageViewModel.ts
+++ deploy/htdocs/version/src/inc/script/app/page/index/IndexPageViewModel.ts
@@ -3,12 +3,21 @@ import IndexPageController from "app/page/index/IndexPageController";
 import ko = require('knockout');
 import ButtonSize from "../../data/enum/layout/ButtonSize";
 import PanelBlocks from "../../data/enum/block/PanelBlocks";
+import DataManager from "../../data/DataManager";
 
 class IndexPageViewModel extends DefaultPageViewModel<IndexPageController>
 {
 	public ButtonSize:Class = ButtonSize;
-	public PanelBlocks:Class = PanelBlocks;
 	public startAdvertisingDisabled:KnockoutObservable<boolean> = ko.observable(false);
+
+	/**
+	 * @public
+	 * @method handleStartAdvertisingClick
+	 */
+	public handleStartAdvertisingClick():void
+	{
+		DataManager.getInstance().panelController.transitionIn(PanelBlocks.CONTACT);
+	}
 
 	/**
 	 *	Overrides AbstractPageViewModel.destruct()
