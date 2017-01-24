eddc68d7a21c62846722d98f23bdcf698ffa6dfb
ricco <ricco@mediamonks.com>
2017-01-24 10:36:55 +0100

    Tweak MarketMap on Small Breakpoint.

--- deploy/htdocs/version/src/inc/script/app/component/block/block-market-map/BlockMarketMapViewModel.ts
+++ deploy/htdocs/version/src/inc/script/app/component/block/block-market-map/BlockMarketMapViewModel.ts
@@ -9,6 +9,57 @@ import DataManager from "../../../data/DataManager";
 class BlockMarketMapViewModel extends DefaultComponentViewModel<BlockMarketMapController, IBlockMarketMapOptions>
 {
 
+	public sidePanelIsOpen:KnockoutObservable<boolean> = ko.observable(false);
+	public selectedState:KnockoutObservable<{id:number;value:string;}> = ko.observable(null);
+
+	// todo fetch from api or json?
+	public stateList:KnockoutObservableArray<{id:number;value:string}> = ko.observableArray([
+		{
+			value: "New York City, NY",
+			id: 0
+		},
+		{
+			value: "Binghamton, NY",
+			id: 1
+		},
+		{
+			value: "Buffalo, NY",
+			id: 2
+		},
+		{
+			value: "Elmira, NY",
+			id: 3
+		},
+		{
+			value: "Watertown, NY",
+			id: 4
+		},
+		{
+			value: "Rochester, NY",
+			id: 5
+		},
+		{
+			value: "Syracuse, NY",
+			id: 6
+		},
+		{
+			value: "Utica, NY",
+			id: 7
+		},
+		{
+			value: "Elmira, NY",
+			id: 8
+		},
+		{
+			value: "Watertown, NY",
+			id: 9
+		},
+		{
+			value: "Rochester, NY",
+			id: 10
+		}
+	]);
+
 	/**
 	 * @public
 	 * @method handleServiceClick
@@ -16,6 +67,24 @@ class BlockMarketMapViewModel extends DefaultComponentViewModel<BlockMarketMapCo
 	public handleServiceClick():void
 	{
 		DataManager.getInstance().panelController.transitionIn(PanelBlocks.CONTACT);
+	}
+
+	/**
+	 * @public
+	 * @method toggleSidePanel
+	 */
+	public toggleSidePanel():void
+	{
+		this.sidePanelIsOpen(!this.sidePanelIsOpen());
+	}
+
+	/**
+	 * @public
+	 * @method onStateSelect
+	 */
+	public onStateSelect(data:{id:number;value:string}):void
+	{
+		this.selectedState(data);
 	}
 
 	/**
