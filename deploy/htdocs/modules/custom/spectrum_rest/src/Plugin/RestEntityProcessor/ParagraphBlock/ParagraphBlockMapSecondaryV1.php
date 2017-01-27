4f5dded1e25244987b813a83cf10c83d696bd89e
Jose Sanchez <jose.sanchez@mediamonks.com>
2017-01-27 17:31:47 -0300

    Add map secondary block.

---
+++ deploy/htdocs/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/ParagraphBlock/ParagraphBlockMapSecondaryV1.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\spectrum_rest\Plugin\RestEntityProcessor\ParagraphBlock;
+
+use Drupal\spectrum_rest\Plugin\SpectrumRestEntityProcessorBase;
+
+/**
+ * Returns the structured data of an entity.
+ *
+ * @RestEntityProcessor(
+ *   id = "spectrum_rest_paragraph_blockmapsecondary_v1",
+ *   label = @Translation("Paragraph: blockmapsecondary"),
+ *   version = "v1",
+ *   entity_type = "paragraph",
+ *   bundle = "blockmapsecondary",
+ *   view_mode = "default"
+ * )
+ */
+class ParagraphBlockMapSecondaryV1 extends SpectrumRestEntityProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getItemData($entity) {
+
+    $data = parent::getCommonData($entity);
+
+    $data = [
+      "id" => 'blockMapSecondary',
+      "data" => $data + [
+        "paragraph" => $this->fieldProcessor->getFieldData($entity->get('field_paragraph')),
+        "subHeading" => $this->fieldProcessor->getFieldData($entity->get('field_subheading')),
+        "heading" => $this->fieldProcessor->getFieldData($entity->get('field_heading')),
+      ],
+    ];
+
+    return $data;
+  }
+
+}
