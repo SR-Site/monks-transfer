00a9c709967f4bfe37ff52915dac04fff076aaf6
Jose Sanchez <jose.sanchez@mediamonks.com>
2017-01-27 17:35:05 -0300

    Add market map block.

---
+++ deploy/htdocs/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/ParagraphBlock/ParagraphBlockMarketMapV1.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\spectrum_rest\Plugin\RestEntityProcessor\ParagraphBlock;
+
+use Drupal\spectrum_rest\Plugin\SpectrumRestEntityProcessorBase;
+
+/**
+ * Returns the structured data of an entity.
+ *
+ * @RestEntityProcessor(
+ *   id = "spectrum_rest_paragraph_blockmarketmap_v1",
+ *   label = @Translation("Paragraph: blockmarketmap"),
+ *   version = "v1",
+ *   entity_type = "paragraph",
+ *   bundle = "blockmarketmap",
+ *   view_mode = "default"
+ * )
+ */
+class ParagraphBlockMarketMapV1 extends SpectrumRestEntityProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getItemData($entity) {
+
+    $data = parent::getCommonData($entity);
+
+    $data = [
+      "id" => 'blockMarketMap',
+      "data" => $data,
+    ];
+
+    return $data;
+  }
+
+}
