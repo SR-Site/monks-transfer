1bbfedf10b40d3c70cc57e428abe52d4c0f0bb8b
Jose Sanchez <jose.sanchez@mediamonks.com>
2017-01-30 14:25:33 -0300

    Add network overview block.

---
+++ deploy/htdocs/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/ParagraphBlock/ParagraphBlockNetworkOverviewV1.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\spectrum_rest\Plugin\RestEntityProcessor\ParagraphBlock;
+
+use Drupal\spectrum_rest\Plugin\SpectrumRestEntityProcessorBase;
+
+/**
+ * Returns the structured data of an entity.
+ *
+ * @RestEntityProcessor(
+ *   id = "spectrum_rest_paragraph_blocknetworkoverview_v1",
+ *   label = @Translation("Paragraph: blocknetworkoverview"),
+ *   version = "v1",
+ *   entity_type = "paragraph",
+ *   bundle = "blocknetworkoverview",
+ *   view_mode = "default"
+ * )
+ */
+class ParagraphBlockNetworkOverviewV1 extends SpectrumRestEntityProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getItemData($entity) {
+
+    $data = parent::getCommonData($entity);
+
+    $data = [
+      "id" => 'blockNetworkOverview',
+      "data" => $data,
+    ];
+
+    return $data;
+  }
+
+}
