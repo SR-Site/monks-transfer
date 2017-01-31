e77fd638d6ccdbbd5d7b295b970abfc402eff92c
Jose Sanchez <jose.sanchez@mediamonks.com>
2017-01-31 16:26:43 -0300

    Add Small heading and Small image blocks.

---
+++ deploy/htdocs/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/ParagraphBlock/ParagraphBlockSmallHeadingV1.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\spectrum_rest\Plugin\RestEntityProcessor\ParagraphBlock;
+
+use Drupal\spectrum_rest\Plugin\SpectrumRestEntityProcessorBase;
+
+/**
+ * Returns the structured data of an entity.
+ *
+ * @RestEntityProcessor(
+ *   id = "spectrum_rest_paragraph_blocksmallheading_v1",
+ *   label = @Translation("Paragraph: blocksmallheading"),
+ *   version = "v1",
+ *   entity_type = "paragraph",
+ *   bundle = "blocksmallheading",
+ *   view_mode = "default"
+ * )
+ */
+class ParagraphBlockSmallHeadingV1 extends SpectrumRestEntityProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getItemData($entity) {
+
+    $data = parent::getCommonData($entity);
+
+    $data = [
+      "id" => 'blockSmallHeading',
+      "data" => $data + [
+        "heading" => $this->fieldProcessor->getFieldData($entity->get('field_heading')),
+      ],
+    ];
+
+    return $data;
+  }
+
+}
