2eebbb95b4c181ac116a034284cb6e4938b9617a
Jose Sanchez <jose.sanchez@mediamonks.com>
2017-01-31 16:54:48 -0300

    Add Text block.

---
+++ deploy/htdocs/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/ParagraphBlock/ParagraphBlockTextV1.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\spectrum_rest\Plugin\RestEntityProcessor\ParagraphBlock;
+
+use Drupal\spectrum_rest\Plugin\SpectrumRestEntityProcessorBase;
+
+/**
+ * Returns the structured data of an entity.
+ *
+ * @RestEntityProcessor(
+ *   id = "spectrum_rest_paragraph_blocktext_v1",
+ *   label = @Translation("Paragraph: blocktext"),
+ *   version = "v1",
+ *   entity_type = "paragraph",
+ *   bundle = "blocktext",
+ *   view_mode = "default"
+ * )
+ */
+class ParagraphBlockTextV1 extends SpectrumRestEntityProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getItemData($entity) {
+
+    $data = parent::getCommonData($entity);
+
+    $data = [
+      "id" => 'blockText',
+      "data" => $data + [
+        "html" => $this->fieldProcessor->getFieldData($entity->get('field_paragraph')),
+      ],
+    ];
+
+    return $data;
+  }
+
+}
