591747e8026a7dc0dc752f411bbac9d7da434217
Lars van Braam <lars@mediamonks.com>
2017-01-31 14:45:11 +0100

    Added more transitions

--- deploy/htdocs/version/src/inc/script/app/component/block/block-image-carousel/BlockImageCarouselTransitionController.ts
+++ deploy/htdocs/version/src/inc/script/app/component/block/block-image-carousel/BlockImageCarouselTransitionController.ts
@@ -1,8 +1,34 @@
 import DefaultTransitionController from "app/util/component-transition/DefaultTransitionController";
+import DataManager from "../../../data/DataManager";
+import {DeviceState} from "../../../data/scss-shared/MediaQueries";
 
 class BlockImageCarouselTransitionController extends DefaultTransitionController
 {
+	/**
+	 * @public
+	 * @method setupTransitionInTimeline
+	 * @description overwrite this method in the parent class
+	 * */
+	protected setupTransitionInTimeline(): void
+	{
+		const pagination = this.element.querySelector('.pagination');
+		const mobilePagination = this.element.querySelector('.component-paginator-dashed');
 
+		this.transitionInTimeline.from(this.element.querySelector('.slider-viewport'), 1, {
+			opacity: 0
+		});
+
+		if(pagination && DataManager.getInstance().deviceStateTracker.currentState() > DeviceState.SMALL)
+		{
+			this.transitionInTimeline.from(pagination, 1, {
+				opacity: 0
+			})
+		}
+		else if(mobilePagination && DataManager.getInstance().deviceStateTracker.currentState() <= DeviceState.SMALL)
+		{
+			this.transitionInTimeline.add(this.getSubTimeline(mobilePagination));
+		}
+	}
 }
 
 export default BlockImageCarouselTransitionController;
--- deploy/htdocs/version/src/inc/script/app/component/paginator-dashed/PaginatorDashedTransitionController.ts
+++ deploy/htdocs/version/src/inc/script/app/component/paginator-dashed/PaginatorDashedTransitionController.ts
@@ -2,7 +2,17 @@ import DefaultTransitionController from "app/util/component-transition/DefaultTr
 
 class PaginatorDashedTransitionController extends DefaultTransitionController
 {
-
+	/**
+	 * @public
+	 * @method setupTransitionInTimeline
+	 * @description overwrite this method in the parent class
+	 * */
+	protected setupTransitionInTimeline(): void
+	{
+		this.transitionInTimeline.from(this.element, 1, {
+			opacity: 0
+		})
+	}
 }
 
 export default PaginatorDashedTransitionController;
