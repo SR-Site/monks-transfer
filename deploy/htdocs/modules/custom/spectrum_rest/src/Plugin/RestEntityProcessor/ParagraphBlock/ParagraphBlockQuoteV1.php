01d8f8772d6eadbffe22e71ef9a3524867dfe736
Jose Sanchez <jose.sanchez@mediamonks.com>
2017-01-31 16:02:46 -0300

    Add Quote and Program module blocks.

---
+++ deploy/htdocs/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/ParagraphBlock/ParagraphBlockQuoteV1.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\spectrum_rest\Plugin\RestEntityProcessor\ParagraphBlock;
+
+use Drupal\spectrum_rest\Plugin\SpectrumRestEntityProcessorBase;
+
+/**
+ * Returns the structured data of an entity.
+ *
+ * @RestEntityProcessor(
+ *   id = "spectrum_rest_paragraph_blockquote_v1",
+ *   label = @Translation("Paragraph: blockquote"),
+ *   version = "v1",
+ *   entity_type = "paragraph",
+ *   bundle = "blockquote",
+ *   view_mode = "default"
+ * )
+ */
+class ParagraphBlockQuoteV1 extends SpectrumRestEntityProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getItemData($entity) {
+
+    $data = parent::getCommonData($entity);
+
+    $data = [
+      "id" => 'blockQuote',
+      "data" => $data + [
+        "description" => $this->fieldProcessor->getFieldData($entity->get('field_paragraph')),
+        "name" => $this->fieldProcessor->getFieldData($entity->get('field_title')),
+        "quote" => $this->fieldProcessor->getFieldData($entity->get('field_label')),
+        "image" => $this->image($entity->get('field_image')),
+      ],
+    ];
+
+    return $data;
+  }
+
+}
