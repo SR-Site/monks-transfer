3b6aab11b40f0db47bac117b4eddee3ae3cd2355
Lars van Braam <lars@mediamonks.com>
2017-02-06 12:58:19 +0100

    Wrapped the menu in a overflow hidden wrapper

--- deploy/htdocs/version/src/inc/script/app/component/block/block-market-map/BlockMarketMapController.ts
+++ deploy/htdocs/version/src/inc/script/app/component/block/block-market-map/BlockMarketMapController.ts
@@ -12,6 +12,7 @@ import Map = mapboxgl.Map;
 import BackgroundLayout = mapboxgl.BackgroundLayout;
 import FillLayout = mapboxgl.FillLayout;
 import GeoJSONSource = mapboxgl.GeoJSONSource;
+import FeatureCollection = GeoJSON.FeatureCollection;
 
 class BlockMarketMapController extends AbstractBlockComponentController<BlockMarketMapViewModel, IBlockMarketMapOptions>
 {
@@ -46,28 +47,7 @@ class BlockMarketMapController extends AbstractBlockComponentController<BlockMar
 			style: 'mapbox://styles/larsvanbraam/ciyodzuy800ds2sla6tuazga1'
 		});
 
-		this._map.on('load', this.handleMapLoad.bind(this))
-
-		// When a click event occurs near a polygon, open a popup at the location of
-		// the feature, with description HTML from its properties.
-		// this._map.on('click', function (e) {
-		// 	var features = this._map.queryRenderedFeatures(e.point, { layers: ['states-layer'] });
-		// 	if (!features.length) {
-		// 		return;
-		// 	}
-		//
-		// 	var feature = features[0];
-		//
-		// 	var popup = new mapboxgl.Popup()
-		// 		.setLngLat(this._map.unproject(e.point))
-		// 		.setHTML(feature.properties.name)
-		// 		.addTo(this._map);
-		// });
-		//
-		// this._map.on('mousemove', function (e) {
-		// 	var features = this._map.queryRenderedFeatures(e.point, { layers: ['states-layer'] });
-		// 	this._map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
-		// });
+		this._map.on('load', this.handleMapLoad.bind(this));
 	}
 
 	/**
@@ -77,43 +57,83 @@ class BlockMarketMapController extends AbstractBlockComponentController<BlockMar
 	private handleMapLoad(): void
 	{
 		this.loadFeatures()
-			.then((data: any) =>
+			.then((data: FeatureCollection) => this.addDataLayer(data))
+			.then(()=> this.addMapEvents());
+	}
+
+	/**
+	 * @private
+	 * @method addDataLayer
+	 * @param data
+	 */
+	private addDataLayer(data:FeatureCollection): void
+	{
+			// Parse it to multi-polygon to make it work... dafuq
+			data.features.forEach((feature) =>
 			{
-				// Parse it to multi-polygon to make it work... dafuq
-				data.features.forEach((feature) =>
-				{
-					feature.geometry.type = 'MultiPolygon';
-					feature.geometry.coordinates = feature.geometry.coordinates.map((coordinate) => [coordinate]);
-				});
+				feature.geometry.type = 'MultiPolygon';
+				feature.geometry.coordinates = feature.geometry.coordinates.map((coordinate) => [coordinate]);
+			});
 
-				// Add the source to the map
-				this._map.addSource('markets', {
-					"type": "geojson",
-					"data": data
-				});
+			// Add the source to the map
+			this._map.addSource('markets', {
+				"type": "geojson",
+				"data": data
+			});
 
-				// Create a  layer
-				let fill = this._map.addLayer({
-					id: 'markets-fill',
-					type: 'fill',
-					source: 'markets',
-					paint: {
-						'fill-pattern': 'stripe-pattern-2'
-						// 'fill-color': '#009bdb',
-						// 'fill-opacity': 0.3
-					}
-				})
+			// Create a  layer
+			let fill = this._map.addLayer({
+				id: 'markets-fill',
+				type: 'fill',
+				source: 'markets',
+				paint: {
+					// 'fill-pattern': 'stripe-pattern-2'
+					'fill-color': '#009bdb',
+					'fill-opacity': 0.3
+				}
+			});
 
-				let outline = this._map.addLayer({
-					id: 'markets-outline',
-					type: 'line',
-					source: 'markets',
-					paint: {
-						'line-width': 2,
-						'line-color': '#009bdb'
-					}
-				});
-			})
+			let outline = this._map.addLayer({
+				id: 'markets-outline',
+				type: 'line',
+				source: 'markets',
+				paint: {
+					'line-width': 2,
+					'line-color': '#009bdb'
+				}
+			});
+	}
+
+	/**
+	 * @private
+	 * @method addMapEvents
+	 */
+	private addMapEvents(): void
+	{
+		// When a click event occurs near a polygon, open a popup at the location of
+		// the feature, with description HTML from its properties.
+		this._map.on('click', function(e)
+		{
+			let features = this._map.queryRenderedFeatures(e.point, {layers: ['markets-fill']});
+
+			if(!features.length)
+			{
+				return;
+			}
+
+			let feature = features[0];
+
+			let popup = new mapboxgl.Popup()
+				.setLngLat(this._map.unproject(e.point))
+				.setHtml(feature.properties.name)
+				.addTo(this._map);
+		});
+
+		this._map.on('mousemove', function(e)
+		{
+			let features = this._map.queryRenderedFeatures(e.point, {layers: ['markets-fill']});
+			this._map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+		});
 	}
 
 
@@ -122,9 +142,9 @@ class BlockMarketMapController extends AbstractBlockComponentController<BlockMar
 	 * @method loadFeatures
 	 * @returns Promise<Array<IFeatures>>
 	 */
-	private loadFeatures(): Promise<Array<IFeature>>
+	private loadFeatures(): Promise<FeatureCollection>
 	{
-		return new Promise((resolve: (features: Array<IFeature>) => void) =>
+		return new Promise((resolve: (data: FeatureCollection) => void) =>
 		{
 			// type: 'geojson',
 			// data: 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_1_states_provinces_shp.geojson'
