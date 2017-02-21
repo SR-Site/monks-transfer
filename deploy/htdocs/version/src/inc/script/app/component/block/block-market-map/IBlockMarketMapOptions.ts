8b052a8353794a10429a2b225252bb5873bb6a75
Lars van Braam <lars@mediamonks.com>
2017-02-21 08:09:28 +0100

    Add typing for the marketMap interface

--- deploy/htdocs/version/src/inc/script/app/component/block/block-market-map/IBlockMarketMapOptions.ts
+++ deploy/htdocs/version/src/inc/script/app/component/block/block-market-map/IBlockMarketMapOptions.ts
@@ -1,7 +1,34 @@
 import IAbstractBlockComponentOptions from "../IAbstractBlockComponentOptions";
+import IImage from "../../../data/interface/media/IImage";
 
-interface IBlockMarketMapOptions extends IAbstractBlockComponentOptions {
-
+interface IBlockMarketMapOptions extends IAbstractBlockComponentOptions
+{
+	/**
+	 * @property
+	 * @description The service displayed in the right bottom corner
+	 */
+	service: {
+		/**
+		 * @property
+		 * @description The name of the service person
+		 */
+		name: string;
+		/**
+		 * @property
+		 * @description The role of the service person
+		 */
+		role: string;
+		/**
+		 * @property
+		 * @description The image of the service person
+		 */
+		image: IImage;
+	};
+	/**
+	 * @property
+	 * @description The label of the search button
+	 */
+	searchLabel: string;
 }
 
 export default IBlockMarketMapOptions;
