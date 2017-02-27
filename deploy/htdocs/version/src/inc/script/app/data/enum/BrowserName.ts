108a5bfe6bd0d4a670acc794ec09ef13b268e42c
Lars van Braam <lars@mediamonks.com>
2017-02-27 08:49:08 +0100

    Fixed IE issue with the header
    ready
    #229

--- deploy/htdocs/version/src/inc/script/app/data/enum/BrowserName.ts
+++ deploy/htdocs/version/src/inc/script/app/data/enum/BrowserName.ts
@@ -4,7 +4,7 @@ class BrowserName
 	public static FIREFOX:string = 'firefox';
 	public static SAFARI:string = 'safari';
 	public static OPERA:string = 'opera';
-	public static INTERNET_EXPLORER:string = 'ie';
+	public static INTERNET_EXPLORER:string = 'internet explorer';
 	public static EDGE:string = 'edge';
 }
 
