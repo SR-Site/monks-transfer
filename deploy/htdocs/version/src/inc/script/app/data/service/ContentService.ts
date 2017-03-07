08f92ac124a382a5932374a268427fe22dd366b9
Lars van Braam <lars@mediamonks.com>
2017-03-07 08:47:14 +0100

    Strip out the first / from a endpoint

--- deploy/htdocs/version/src/inc/script/app/data/service/ContentService.ts
+++ deploy/htdocs/version/src/inc/script/app/data/service/ContentService.ts
@@ -55,8 +55,11 @@ class ContentService extends AbstractService
 	 */
 	public loadMore(endpoint: string, offset: number, limit: number, filter: {[filterType: string]: string}): Promise<IGatewayResult<{blocks: Array<IBlock>}>>
 	{
-		// TODO: Strip out the /api/v1 part, backend should not return it though!
-		endpoint = endpoint.replace('/api/v1/', '');
+		if(endpoint.indexOf('/') === 0)
+		{
+			// Strip out the first "/"
+			endpoint = endpoint.substring(1);
+		}
 
 		return this.gateway.get(
 			endpoint,
