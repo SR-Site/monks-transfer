0ecd8f1f278aa1b39ad7751be7fdc88e378668cd
Jose Sanchez <jose.sanchez@mediamonks.com>
2017-03-08 14:21:56 -0300

    Add fields Read time and view count in the blogPost block. Update author property with real content. Add fields, Name and Role to the user entity.
    Test: #284

--- deploy/htdocs/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/Node/NodeArticleTeaserV1.php
+++ deploy/htdocs/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/Node/NodeArticleTeaserV1.php
@@ -27,6 +27,8 @@ class NodeArticleTeaserV1 extends SpectrumRestEntityProcessorBase {
       "social" => $this->getItems($entity->get('field_links')),
       "author" => $this->entityProcessor->getEntityData($entity->getRevisionAuthor(), 'v1'),
       "tags" => $this->getTags($entity),
+      "views" => 0,
+      "time" => $this->fieldProcessor->getFieldData($entity->get('field_read_time')) . ":00",
       "date" => $this->dateFormatter->format($entity->getCreatedTime(), 'custom', 'd/m/Y'),
       "paragraph" => $this->fieldProcessor->getFieldData($entity->get('field_content')),
       "subheading" => $this->fieldProcessor->getFieldData($entity->get('field_excerpt')),
