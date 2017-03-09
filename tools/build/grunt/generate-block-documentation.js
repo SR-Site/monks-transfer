c7a0cc62ad2295a78cac09c7fa6995223de6a82d
Lars van Braam <lars@mediamonks.com>
2017-03-09 09:55:13 +0100

    Replaced block documentation with the node module

--- tools/build/grunt/generate-block-documentation.js
+++ tools/build/grunt/generate-block-documentation.js
@@ -2,13 +2,9 @@ module.exports = function( grunt, options )
 {
 	return {
 		options: {
-			'input': '<%= sourceDir %>inc/script/app/component/block',
+			'input': '<%= sourceDir %>inc/script/app/component/block/',
 			'output': '<%= base %>tools/block-documentation/',
-			'placeholderValues': {
-				'string': 'Lorem ipsum dolor sit amet',
-				'boolean': true,
-				'number': 1
-			}
+			'exampleBlockIdLabel': 'id'
 		},
 		default: {}
 	};
