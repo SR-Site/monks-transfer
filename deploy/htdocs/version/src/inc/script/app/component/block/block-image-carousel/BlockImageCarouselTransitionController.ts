2b66999683d19f40f8f85f1bc46f2db9058acb78
Lars van Braam <lars@mediamonks.com>
2017-03-15 09:37:22 +0100

    Add video support for the image carousel Update documentation

--- deploy/htdocs/version/src/inc/script/app/component/block/block-image-carousel/BlockImageCarouselTransitionController.ts
+++ deploy/htdocs/version/src/inc/script/app/component/block/block-image-carousel/BlockImageCarouselTransitionController.ts
@@ -12,12 +12,18 @@ class BlockImageCarouselTransitionController extends AbstractTransitionControlle
 	{
 		const pagination = this.element.querySelector('.pagination');
 		const mobilePagination = this.element.querySelector('.component-paginator-dashed');
+		const playButtons = Array.prototype.slice.call(this.element.querySelectorAll('.component-button-play-circle'));
 
 		this.transitionInTimeline.from(this.element.querySelector('.slider-viewport'), 1, {
 			opacity: 0
 		});
 
 		this.transitionInTimeline.add(this.getSubTimeline(mobilePagination));
+
+		playButtons.forEach((playButton) =>
+		{
+			this.transitionInTimeline.add(this.getSubTimeline(playButton), 0);
+		})
 	}
 }
 
