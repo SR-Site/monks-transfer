407d2fc1b5d01d64435599140fb128a60075f355
Lars van Braam <lars@mediamonks.com>
2017-03-20 14:07:25 +0100

    Add transition in for the text block

--- deploy/htdocs/version/src/inc/script/app/component/block/block-text/BlockTextTransitionController.ts
+++ deploy/htdocs/version/src/inc/script/app/component/block/block-text/BlockTextTransitionController.ts
@@ -3,7 +3,15 @@ import BlockTextController from "./BlockTextController";
 
 class BlockTextTransitionController extends AbstractTransitionController<BlockTextController>
 {
-
+	/**
+	 * @public
+	 * @method setupTransitionInTimeline
+	 * @description overwrite this method in the parent class
+	 * */
+	protected setupTransitionInTimeline(): void
+	{
+		this.transitionInTimeline.from(this.element, 0.8, {y: 50, autoAlpha: 0, ease: Expo.easeOut});
+	}
 }
 
 export default BlockTextTransitionController;
