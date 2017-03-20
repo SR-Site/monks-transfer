405c7c1e1f62643b691e8ce9c5e340f7822e184d
Lars van Braam <lars@mediamonks.com>
2017-03-20 14:04:03 +0100

    Add transition in for the small heading

--- deploy/htdocs/version/src/inc/script/app/component/block/block-small-heading/BlockSmallHeadingTransitionController.ts
+++ deploy/htdocs/version/src/inc/script/app/component/block/block-small-heading/BlockSmallHeadingTransitionController.ts
@@ -3,7 +3,15 @@ import BlockSmallHeadingController from "./BlockSmallHeadingController";
 
 class BlockSmallHeadingTransitionController extends AbstractTransitionController<BlockSmallHeadingController>
 {
-
+	/**
+	 * @public
+	 * @method setupTransitionInTimeline
+	 * @description overwrite this method in the parent class
+	 * */
+	protected setupTransitionInTimeline(): void
+	{
+		this.transitionInTimeline.from(this.element.querySelector('.heading'), 0.8, { y: 50, autoAlpha: 0, ease: Expo.easeOut });
+	}
 }
 
 export default BlockSmallHeadingTransitionController;
