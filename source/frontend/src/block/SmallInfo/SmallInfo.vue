7fa1a3fd24d49374a543e61cad74da7523ff7468
Lars van Braam <lars@mediamonks.com>
2017-10-23 16:23:17 +0200

    Add the small info block

--- source/frontend/src/block/SmallInfo/SmallInfo.vue
+++ source/frontend/src/block/SmallInfo/SmallInfo.vue
@@ -2,7 +2,28 @@
 <script src="./SmallInfo.js"></script>
 
 <template>
-	<div>
-		<h2>SmallInfo</h2>
+	<div :class="$style.smallInfo">
+		<div class="site-frame" :class="$style.siteFrame">
+			<div class="overrule-overlap" :class="$style.overruleOverlap">
+				<div :class="$style.introduction">
+					<h4 :class="$style.heading" class="heading heading-04" v-html="data.heading" ref="heading"></h4>
+				</div>
+				<div :class="$style.description">
+					<p :class="$style.copy" class="copy copy-01" v-html="data.paragraph" ref="copy"></p>
+					<ButtonPrimary
+						componentId="ButtonPrimary"
+						v-if="data.link"
+						:title="data.link.title"
+						:label="data.link.label"
+						:type="ButtonType.LINK"
+						:theme="Theme.DARK"
+						:link="{
+							type: linkTypeMap[data.link.type],
+							target: data.link.target,
+						}"
+						:class="$style.button"/>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
--- source/frontend/src/block/SmallInfo/SmallInfoTransitionController.ts
+++ source/frontend/src/block/SmallInfo/SmallInfoTransitionController.ts
@@ -1,4 +1,5 @@
 import { AbstractTransitionController } from 'vue-transition-component';
+import { Expo } from 'gsap';
 
 class SmallInfoTransitionController extends AbstractTransitionController {
 	/**
@@ -6,13 +7,52 @@ class SmallInfoTransitionController extends AbstractTransitionController {
 	 * @method setupTransitionInTimeline
 	 * @description Use this method to setup your transition in timeline
 	 * */
-	protected setupTransitionInTimeline(): void {}
+	protected setupTransitionInTimeline(): void {
+		this.transitionInTimeline.fromTo(
+			this.viewModel.$refs.heading,
+			0.8,
+			{
+				y: 50,
+				autoAlpha: 0,
+			},
+			{
+				y: 0,
+				autoAlpha: 1,
+				ease: Expo.easeOut,
+			},
+			'=+0.5',
+		);
+
+		if (this.viewModel.$refs.copy) {
+			this.transitionInTimeline.fromTo(
+				this.viewModel.$refs.copy,
+				0.8,
+				{
+					y: 50,
+					autoAlpha: 0,
+				},
+				{
+					y: 0,
+					autoAlpha: 1,
+					ease: Expo.easeOut,
+				},
+				'=-0.7',
+			);
+		}
+
+		if (this.viewModel.hasChild('ButtonPrimary')) {
+			this.transitionInTimeline.add(
+				this.getSubTimeline('ButtonPrimary'),
+				'=-0.7',
+			);
+		}
+	}
 
 	/**
-	* @public
-	* @method setupTransitionOutTimeline
-	* @description Use this method to setup your transition out timeline
-	* */
+	 * @public
+	 * @method setupTransitionOutTimeline
+	 * @description Use this method to setup your transition out timeline
+	 * */
 	protected setupTransitionOutTimeline(): void {}
 }
 
