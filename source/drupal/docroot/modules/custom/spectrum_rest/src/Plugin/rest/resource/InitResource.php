95f9b5ae1313ac09b01b581ba9aa51fe838226cb
Mladen Stankovic <mladen.stankovic@mediamonks.com>
2017-11-09 14:57:32 +0100

    Updated main menu block

--- source/drupal/docroot/modules/custom/spectrum_rest/src/Plugin/rest/resource/InitResource.php
+++ source/drupal/docroot/modules/custom/spectrum_rest/src/Plugin/rest/resource/InitResource.php
@@ -314,11 +314,23 @@ class InitResource extends ResourceBase {
         "type" => UrlHelper::isExternal($url) ? self::MENU_LINK_EXTERNAL : self::MENU_LINK_INTERNAL,
       ];
 
-      // Temporally solution.
-      if ($menu == 'main') {
+      // Display child items.
+      if (!empty($menu_item->subtree) && $menu == 'main') {
         $parsed_array = [
-          "link" => $parsed_array,
+          'section' => $link->getTitle(),
+          'links' => [
+            [
+              "label" => $link->getTitle(),
+              "title" => $link->getTitle(),
+              "target" => $url,
+              "type" => UrlHelper::isExternal($url) ? self::MENU_LINK_EXTERNAL : self::MENU_LINK_INTERNAL,
+            ]
+          ]
         ];
+        foreach ($menu_item->subtree as $subItem) {
+          /** @var \Drupal\Core\Menu\MenuLinkTreeElement $subItem */
+          $parsed_array['links'][] = $this->prepareMenuLink($subItem, $menu);
+        }
       }
     }
 
