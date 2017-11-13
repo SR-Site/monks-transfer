55b800ac056cfedd8d7b0d518133971d54b6749a
Lars van Braam <lars@mediamonks.com>
2017-11-13 16:17:18 +0100

    Add the show detail block

--- source/frontend/src/block/ShowDetail/ShowDetail.vue
+++ source/frontend/src/block/ShowDetail/ShowDetail.vue
@@ -2,7 +2,30 @@
 <script src="./ShowDetail.js"></script>
 
 <template>
-	<div>
-		<h2>ShowDetail</h2>
+	<div :class="$style.showDetail">
+		<div class="site-frame">
+			<div class="overrule-overlap" :class="$style.content">
+				<h2 class="heading heading-02" v-html="data.heading" :class="$style.heading" ref="header"></h2>
+				<p class="copy copy-01" v-html="data.paragraph" :class="$style.copy" ref="copy"></p>
+				<ul :class="$style.details">
+					<li :class="$style.detail" ref="network">
+						<h3 class="heading heading-07" v-html="data.network.label" :class="$style.detailHeading"></h3>
+						<figure :class="$style.detailImage">
+							<ResponsiveImage :image="data.network.image" class="abs-fill fit-cover"/>
+						</figure>
+					</li>
+					<li :class="$style.detail" ref="airTime">
+						<h3 class="heading heading-07" v-html="data.airTime.label" :class="$style.detailHeading"></h3>
+						<p class="copy copy-01" :class="$style.detailValue" v-html="data.airTime.value"></p>
+					</li>
+					<li :class="$style.detail" ref="ageRestriction">
+						<h3 class="heading heading-07" v-html="data.ageRestriction.label" :class="$style.detailHeading"></h3>
+						<figure :class="$style.detailImage">
+							<ResponsiveImage :image="data.ageRestriction.image" class="abs-fill fit-cover" />
+						</figure>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </template>
--- source/frontend/src/block/ShowDetail/ShowDetailData.js
+++ source/frontend/src/block/ShowDetail/ShowDetailData.js
@@ -1,1 +1,43 @@
-export default {};
+import VueTypes from 'vue-types';
+import PropImage from '../../data/prop-type/media/PropImage';
+
+/**
+ * @param {description} marginTop This defines the amount of spacing at the top of the block
+ * @param {description} overlap This defines if a block overlaps the next block on the layout
+ * @param {description} windowed This defines if a block has extra padding left and right
+ * @param {description} scrollId This unique id is used for scrolling to blocks
+ * @param {placeholder} scrollId unique-block-id
+ * @param {description} network The network value for the show
+ * @param {description} network.label The category label
+ * @param {description} network.image The network logo
+ * @param {description} airTime.label The category label
+ * @param {description} airTime.image The air time value
+ * @param {description} ageRestriction.label The category label
+ * @param {description} ageRestriction.image The age restriction label
+ */
+export default {
+	marginTop: VueTypes.number.isRequired,
+	overlap: VueTypes.bool.isRequired,
+	windowed: VueTypes.bool.isRequired,
+	scrollId: VueTypes.string,
+	heading: VueTypes.string.isRequired,
+	paragraph: VueTypes.string.isRequired,
+	network: VueTypes.shape(
+		{
+			label: VueTypes.string.isRequired,
+			image: VueTypes.shape(PropImage).isRequired,
+		},
+	).isRequired,
+	airTime: VueTypes.shape(
+		{
+			label: VueTypes.string.isRequired,
+			value: VueTypes.string.isRequired,
+		},
+	).isRequired,
+	ageRestriction: VueTypes.shape(
+		{
+			label: VueTypes.string.isRequired,
+			image: VueTypes.shape(PropImage).isRequired,
+		},
+	).isRequired,
+};
