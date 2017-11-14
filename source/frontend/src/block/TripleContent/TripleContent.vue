777c67ac5c90550f2bae984c956103fa805eb50c
Lars van Braam <lars@mediamonks.com>
2017-11-14 09:44:05 +0100

    Add support for images on the triple content block

--- source/frontend/src/block/TripleContent/TripleContent.vue
+++ source/frontend/src/block/TripleContent/TripleContent.vue
@@ -8,6 +8,9 @@
 				<h2 class="heading heading-03" v-html="data.heading" ref="header"></h2>
 				<div :class="$style.tripleContentItems">
 					<div :class="$style.tripleContentItem" v-for="(item, index) in data.items" :key="index" ref="item">
+						<figure v-if="item.image" :class="$style.image">
+							<ResponsiveImage :image="item.image" class="abs-fill fit-cover"/>
+						</figure>
 						<h3 class="heading heading-05" v-html="item.heading" :class="$style.heading"></h3>
 						<p class="copy copy-01" v-html="item.paragraph"></p>
 					</div>
