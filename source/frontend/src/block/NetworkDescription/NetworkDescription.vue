f17dacfda9d00f4eadc5dedf18b7f880e2b6bba9
Lars van Braam <lars@mediamonks.com>
2017-11-16 09:55:20 +0100

    Removed unused blocks

--- source/frontend/src/block/NetworkDescription/NetworkDescription.vue
+++
@@ -1,8 +0,0 @@
-<style src="./NetworkDescription.scss" module lang="scss"></style>
-<script src="./NetworkDescription.js"></script>
-
-<template>
-	<div>
-		<h2>NetworkDescription</h2>
-	</div>
-</template>
