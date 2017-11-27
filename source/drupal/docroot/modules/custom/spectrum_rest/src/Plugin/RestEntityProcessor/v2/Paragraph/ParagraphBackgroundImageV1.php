72db12abb8cfd0c6f4c68a4fcc35dfc2960d9a23
Mladen Stankovic <mladen.stankovic@mediamonks.com>
2017-11-27 11:37:13 +0100

    Added HeroQuaternary block REST API

--- source/drupal/docroot/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/v2/Paragraph/ParagraphBackgroundImageV1.php
+++ source/drupal/docroot/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/v2/Paragraph/ParagraphBackgroundImageV1.php
@@ -2,20 +2,18 @@
 
 namespace Drupal\spectrum_rest\Plugin\RestEntityProcessor\v2\Paragraph;
 
-use Drupal\mm_rest\Plugin\RestEntityProcessorBase;
-
 /**
  * Returns the structured data of an entity.
  *
  * @RestEntityProcessor(
- *   id = "spectrum_rest_paragraph_poster_v1",
- *   label = @Translation("Paragraph: poster"),
+ *   id = "spectrum_rest_paragraph_background_image_v1",
+ *   label = @Translation("Paragraph: Background image"),
  *   version = "v1",
  *   entity_type = "paragraph",
- *   bundle = "poster",
+ *   bundle = "background_image",
  *   view_mode = "default"
  * )
  */
-class ParagraphPosterV1 extends ParagraphImageDefaultV1 {
+class ParagraphBackgroundImageV1 extends ParagraphImageDefaultV1 {
 
 }
