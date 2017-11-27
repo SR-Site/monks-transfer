1e9b9499ed99d502be9ee51a6ba0286257e26490
Mladen Stankovic <mladen.stankovic@mediamonks.com>
2017-11-27 17:48:33 +0100

    Created VideoWithContentC REST API

---
+++ source/drupal/docroot/modules/custom/spectrum_rest/src/Plugin/RestEntityProcessor/v2/ParagraphBlock/ParagraphBlockVideoWithContentCV1.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\spectrum_rest\Plugin\RestEntityProcessor\v2\ParagraphBlock;
+
+use Drupal\spectrum_rest\Plugin\SpectrumRestEntityProcessorBase;
+
+/**
+ * Returns the structured data of an entity.
+ *
+ * @RestEntityProcessor(
+ *   id = "spectrum_rest_paragraph_block_video_with_content_c_v1",
+ *   label = @Translation("Paragraph: Block Video With Content C"),
+ *   version = "v1",
+ *   entity_type = "paragraph",
+ *   bundle = "block_video_with_content_c",
+ *   view_mode = "default"
+ * )
+ */
+class ParagraphBlockVideoWithContentCV1 extends SpectrumRestEntityProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getItemData($entity) {
+    // Get common and heading and paragraph.
+    $data = $this->getCommonData($entity) + $this->getHeadingParagraphData($entity);
+
+    // Get rest of the data.
+    $data = [
+      'id' => 'VideoWithContentC',
+      'data' => $data + [
+        'scrollId' => 'videoWithContent',
+        'video' => $this->fieldProcessor->getFieldData($entity->get('field_video')) ?: [],
+        'poster' => $this->fieldProcessor->getFieldData($entity->get('field_poster')),
+        'link' => $this->fieldProcessor->getFieldData($entity->get('field_link')),
+      ],
+    ];
+
+    return $data;
+  }
+
+}
