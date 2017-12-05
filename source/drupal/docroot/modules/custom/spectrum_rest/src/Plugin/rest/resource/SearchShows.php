01047a90d4e2dbfc0387c1c6d6e240e690ef3a31
Mladen Stankovic <mladen.stankovic@mediamonks.com>
2017-12-05 18:35:38 +0100

    Added show landing page

---
+++ source/drupal/docroot/modules/custom/spectrum_rest/src/Plugin/rest/resource/SearchShows.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\spectrum_rest\Plugin\rest\resource;
+
+use Drupal\mm_rest_search\Plugin\ResourceSearch;
+
+/**
+ * Provides a resource to search page nodes.
+ *
+ * @RestResource(
+ *   id = "spectrum_rest_searchshows_v1",
+ *   version = "v1",
+ *   index_name = "shows",
+ *   view_mode = "search",
+ *   version = "v1",
+ *   label = @Translation("Spectrum rest Search Shows resource"),
+ *   uri_paths = {
+ *     "canonical" = "/api/v1/search/show"
+ *   }
+ * )
+ */
+class SearchShows extends ResourceSearch {
+
+  /**
+   * Paginated response.
+   *
+   * @var \Drupal\mm_rest\Response\OffsetPaginatedResponse
+   */
+  private $offsetResponse;
+
+  /**
+   * Responds to entity GET requests with parameter.
+   *
+   * @return array
+   *   Response.
+   */
+  public function get() {
+
+    $this->offsetResponse = parent::get();
+
+    return $this->responseData();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function responseData() {
+    // Rearrange response.
+    $data = $this->offsetResponse->getData();
+    $this->offsetResponse->setData([
+      'blocks' => isset($data['results']) ? $data['results'] : [],
+    ]);
+
+    return $this->offsetResponse;
+  }
+
+}
