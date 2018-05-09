a947c30adb0a90bab7c0caabc956dbc11a5a7381
davide.zanon <davide.zanon@mediamonks.com>
2018-05-09 11:01:33 +0200

    Inverted divs order inside the file Feedback #696

--- source/frontend/src/block/MarketMap/MarketPanel/block/MarketVideo/MarketVideo.vue
+++ source/frontend/src/block/MarketMap/MarketPanel/block/MarketVideo/MarketVideo.vue
@@ -5,9 +5,9 @@
 
 <template>
 	<div :class="$style.wrapper">
-		<h3 :class="['heading', 'heading-06', $style.heading]" v-if="data.heading">{{data.heading}}</h3>
 		<div :class="$style.videoPlayer">
 			<VideoPlayer componentId="VideoPlayer" class="abs-fill"/>
 		</div>
+		<h3 :class="['heading', 'heading-06', $style.heading]" v-if="data.heading">{{data.heading}}</h3>
 	</div>
 </template>
